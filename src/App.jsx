369b55360f6f3e764f9af7203de3fbafee97a4de
Shaurya Mehrotra <mehrotra.shaurya5@gmail.com>
2025-01-08 14:50:06 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,30 @@ import { useState } from 'react'
 import { set, useForm } from 'react-hook-form'
 import Card from './card'
 import Loader from './Loader'
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser, useAuth } from "@clerk/clerk-react";
+import logo from './assets/supabaseLogo.svg'
+import Intro from './Intro'
 
 let api = 'https://practice-3c610-default-rtdb.asia-southeast1.firebasedatabase.app/'
 
 function App() {
+  let {user} = useUser()
+  let {isSignedIn} = useAuth()
   let {register , handleSubmit , reset} = useForm()
   let [todos, setTodos] = useState([])
   let [submit , setSubmit] = useState(false)
 
   function submitHandler(data){
     setSubmit(true)
+    
+    const requestData = {
+        ...data, 
+        username: user.username 
+    };
 
-  axios.post(`${api}todos.json`,data).then(()=>{
-    console.log(data)
+  axios.post(`${api}todos.json`,requestData).then(()=>{
+    
+    console.log(requestData)
     setSubmit(false)
     reset()
     fetchTodos()
@@ -67,19 +78,39 @@ function App() {
     console.log('deleteCalled')
   }
   
+  
 
   return (
+
     <div>
+      <div className="border-b py-3 ">
+        <div className="max-w-4xl mx-auto flex justify-between items-center px-3">
+          <img className='h-8' src={logo} alt="" />
+        <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+        </div>
+      </div>
+      <SignedIn>
       <div className="mx-auto mt-14 w-80">
-         <h1 className='text-3xl font-black'>TODO LIST <span className='text-neutral-500'>@Shaurya</span></h1>
+         <h1 className='text-3xl font-black'>TODO LIST <span className='text-neutral-500'>@{isSignedIn ? user.firstName : ""}</span></h1>
          <form action="" onSubmit={handleSubmit(submitHandler)}>
               <input {...register('taskName')} className='border mt-6 w-full outline-none px-2 py-1' placeholder='e.g : Learn Javascript' type="text" />
               <input  className='w-full mt-6 rounded-xl px-3 py-1 bg-black text-white' type="submit" value={submit ? <Loader></Loader> : 'Create Your Todo'} />
          </form>
       </div>
       {
-        todos.map((tod)=><Card id={tod.id} deleteHandler={deleteHandler} taskName = {tod.taskName}></Card>)
+        todos.filter(tod=>isSignedIn ? tod.username == user.username : true).map((tod)=><Card id={tod.id} deleteHandler={deleteHandler} taskName = {tod.taskName}></Card>)
       }
+      </SignedIn>
+      <SignedOut>
+            <Intro></Intro>
+      </SignedOut>
       
     </div>
   )
---
+++ src/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='max-w-3xl mx-auto mt-44 text-center'>
+     <h1 className='text-4xl text-neutral-600 font-black'>Organize your work and life, finally.</h1>
+     <p className='text-lg font-light text-neutral-500'>Simplify life for both you and your team. The world’s #1 task manager and to-do list app.</p>
+    </div>
+  )
+}
+
+export default Intro
